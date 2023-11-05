1aad7f47e7c9c6e8cc7722acd0b581e1915089c5
Bartosz Baran <bartosz.baran192@gmail.com>
2023-11-05 19:02:40 +0100

    views blueprint configuration

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = secret_key
 
+    from .views import views
+    app.register_blueprint(views, url_prefix='/')
+
     return app

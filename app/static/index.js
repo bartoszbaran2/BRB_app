afd046a3439423e9c5364ebbda724fd6b8809dc8
Bartosz Baran <bartosz.baran192@gmail.com>
2023-11-07 18:41:22 +0100

    basic cursor movement controlled from clinet by button and space key

---
+++ app/static/index.js
@@ -0,0 +1,37 @@
+document.addEventListener('mousemove', (e) => {
+        const mousePositionElement = document.getElementById('mousePosition')
+        const x = e.clientX;
+        const y = e.clientY;
+        mousePositionElement.textContent = `x=${x}, y=${y}`;
+        })
+
+
+function activateCursor(toggle) {
+    fetch("/start", {
+        method: "POST",
+        body: JSON.stringify({toggle: toggle})
+        }).then((_resp) => {
+            window.location.href = "/";
+        })
+        }
+
+var cursorActive = false;
+
+function toggleCursorButton() {
+    var button = document.getElementById('cursorButton')
+
+    cursorActive = !cursorActive
+
+    if (cursorActive) {
+        button.innerHTML = 'Stop (or press Space)'
+    } else {
+        button.innerHTML = 'Start'
+    }
+    activateCursor(cursorActive)
+}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === ' ') {
+    toggleCursorButton()
+    }
+})
--- app/views.py
+++ app/views.py
@@ -1,8 +1,30 @@
-from flask import Blueprint
+import json
+
+import pyautogui as pg
+from flask import Blueprint, render_template, request, jsonify
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():
-    return '<h1>Home Page</h1>'
+    screen_size = pg.size()
+    return render_template('home.html', screen_size=screen_size)
+
+
+@views.route('/start', methods=['POST'])
+def move_cursor():
+    toggle = json.loads(request.data)['toggle']
+    pg.FAILSAFE_POINTS = [(0, 0)]
+
+    while True:
+        x, y = pg.position()
+        try:
+            if not toggle and pg.onScreen(x, y):
+                pg.FAILSAFE_POINTS.append((x, y))
+                pg.moveTo(x, y)
+
+            pg.move(0, 5)
+
+        except pg.FailSafeException:
+            return jsonify({})

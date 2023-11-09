2c79b89c076cb5f1489178eb4350dd5c30b3f8e4
Bartosz Baran <bartosz.baran192@gmail.com>
2023-11-09 11:40:52 +0100

    testing coverage configuration by customm command

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import os
+import sys
+import click
 
 from app import create_app
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 
 @app.cli.command()
-def test():
+@click.option('--coverage/--no-coverage', default=False, help='Run tests with code coverage.')
+def test(coverage):
+    """Run unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+    if COV:
+        COV.stop()
+        COV.save()
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        cov_dir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=cov_dir)
+        print('Coverage HTML: file://%s/index.html' % cov_dir)
+        COV.erase()

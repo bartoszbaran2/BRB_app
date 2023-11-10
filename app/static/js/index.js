34af6e4bcf7c59dd75ee28ac80cd420323420abb
Bartosz Baran <bartosz.baran192@gmail.com>
2023-11-10 12:50:33 +0100

    timer  feature

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,31 +1,20 @@
-document.addEventListener('mousemove', (e) => {
-        const mousePositionElement = document.getElementById('mousePosition')
-        const x = e.clientX;
-        const y = e.clientY;
-        mousePositionElement.textContent = `x=${x}, y=${y}`;
-        })
-
-
 function activateCursor(toggle) {
     fetch("/start", {
         method: "POST",
         body: JSON.stringify({toggle: toggle})
         }).then((_resp) => {
             window.location.href = "/";
-        })
-        }
+    })
+}
 
 var cursorActive = false;
 
 function toggleCursorButton() {
     var button = document.getElementById('cursorButton')
-
     cursorActive = !cursorActive
 
     if (cursorActive) {
         button.innerHTML = 'Stop (or press Space)'
-    } else {
-        button.innerHTML = 'BRB!'
     }
     activateCursor(cursorActive)
 }
@@ -35,3 +24,4 @@ document.addEventListener('keydown', (e) => {
     toggleCursorButton()
     }
 })
+

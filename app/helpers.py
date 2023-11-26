81a6a6cc90daf4b134e959a646a63bdd132853c6
Bartosz Baran <bartosz.baran192@gmail.com>
2023-11-26 17:14:34 +0100

    refactor "move_cursor" endpoint code

--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,5 @@
 import pyautogui as pg
+from flask import jsonify
 
 
 def cursor_movement():
@@ -8,3 +9,19 @@ def cursor_movement():
     pg.move(-5, 0)
     pg.move(-5, 0)
     pg.move(-5, 0)
+
+
+def handle_cursor_movement(toggle):
+    pg.FAILSAFE_POINTS = [(0, 0)]
+
+    while True:
+        x, y = pg.position()
+        try:
+            if not toggle and pg.onScreen(x, y):
+                pg.FAILSAFE_POINTS.append((x, y))
+                pg.moveTo(x, y)
+
+            cursor_movement()
+
+        except pg.FailSafeException:
+            return jsonify({})
